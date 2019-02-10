578b2f589498d21704c1642810d57ea28fd76742
SukhpalSingh95 <sukhpal.sprojectworks@outlook.com>
2019-02-10 20:01:21 +0000

    Brushing up on Java skills

---
+++ JavaTutorial/src/HelloWorld.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+
+    /*
+    * This is to start off the Java brush up
+    * */
+
+
+    static Scanner scannerObject = new Scanner(System.in); // this allows you to take input from the user via a keyboard
+
+    final double PiValue= 3.141; //final means that the value will not change ever!
+
+
+
+    public static void main(String[] args){
+        /*
+        * Public just means anyone can use this function from this class
+        * static means that this is a function that belongs to the class
+        * void means the function will not return anything that is exectued in the function
+        * a String array will be passed and populated
+        * */
+
+
+        System.out.println("Hello World"); // this is the method for printing out objects to console
+    }
+
+}
